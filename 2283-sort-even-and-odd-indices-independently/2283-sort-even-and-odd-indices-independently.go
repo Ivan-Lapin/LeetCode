b47aeeae8f8c3b94ca9b14324dcfecf50bae138f
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2025-10-21 14:02:43 +0300

    Time: 1 ms (30%), Space: 5.4 MB (20%) - LeetHub

---
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.go
@@ -0,0 +1,37 @@
+func sortEvenOdd(nums []int) []int {
+
+    odd := []int{}
+    even := []int{}
+    result := make([]int, len(nums))
+
+    for i, num := range nums {
+        if i % 2 == 0{
+            even = append(even, num)
+        } else {
+            odd = append(odd, num)
+        }
+    }
+
+    sort.Ints(even)
+    sort.Slice(odd, func(i, j int) bool {
+        return odd[i] > odd[j]
+    })
+
+    fmt.Println(even)
+    fmt.Println(odd)
+
+    for i, step := 0, 0; i < len(odd) && i < len(even); i++{
+        result[i+step] = even[i]
+        step++
+        result[i+step] = odd[i]
+    }
+
+    if len(even) > len(odd) {
+        result[len(result)-1] = even[len(even)-1]
+    } else if len(even) < len(odd) {
+        result[len(result)-1] = odd[len(odd)-1]
+    }
+
+
+    return result
+}
