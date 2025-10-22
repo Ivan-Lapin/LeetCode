a3bc5fdb90bb46f03f4b1c48433c83d96552fca1
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2025-10-22 12:06:29 +0300

    Time: 3 ms (34.62%), Space: 9.2 MB (34.62%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.go
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.go
@@ -5,17 +5,18 @@ func findNonMinOrMax(nums []int) int {
 
     for i := 1; i < len(nums); i++{
         if nums[i] > maxNum {
+            result = maxNum
             maxNum = nums[i]
-        } 
-        if nums[i] < minNum {
+        } else if nums[i] < minNum {
+            result = minNum
             minNum = nums[i]
-        } 
+        } else {
+            result = nums[i]
+        }
     }
 
-    for _, num := range nums {
-        if (num != maxNum) && (num != minNum) {
-            result = num
-        } 
+    if result == minNum || result == maxNum {
+        return -1
     }
 
     return result
