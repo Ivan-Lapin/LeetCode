b0f30ed1a0af79fe0c16876ea4d4d0586147dee8
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2025-10-31 09:46:36 +0300

    Time: 1 ms (32.11%), Space: 4.3 MB (16.51%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.go
@@ -0,0 +1,28 @@
+func maxNumberOfBalloons(text string) int {
+
+    freqChar := make(map[rune]int)
+    count := 0
+
+    for i := 0; i < len(text); i++{
+        freqChar[rune(text[i])] ++
+    }
+
+    var def func(mapa map[rune]int) bool 
+
+    def = func(mapa map[rune]int) bool {
+        for _, ch := range []rune{'b', 'a', 'l', 'l', 'o', 'o', 'n'} {
+            if val, exist := mapa[ch]; exist && val == 0 || !exist{
+                return false
+            } else {
+                mapa[ch] --
+            }
+        }
+        return true
+    }
+
+    for def(freqChar) {
+        count++
+    }
+
+    return count
+}
