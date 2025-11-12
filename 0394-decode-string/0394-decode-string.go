97d66bf3118245b40ae2e74c40317b9b0896aa29
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2025-11-12 10:14:24 +0300

    Time: 0 ms (100%), Space: 4.1 MB (58.67%) - LeetHub

--- 0394-decode-string/0394-decode-string.go
+++ 0394-decode-string/0394-decode-string.go
@@ -9,20 +9,28 @@ func decodeString(s string) string {
 		var output_sub strings.Builder
 
 		for i := index; i < len(s); i++ {
+
 			if s[i] >= 48 && s[i] <= 57 {
+
 				num, next := define_number(i)
+
 				var next_sub string
 				next_sub, next = def(num, next)
+                
 				output_sub.WriteString(next_sub)
 				i = next
+
 				continue
 			}
+
 			if s[i] == ']' {
 				index = i
 				break
 			}
+
 			output_sub.WriteByte(s[i])
 		}
+
 		temp := output_sub.String()
 		for i := 1; i < number; i++ {
 			output_sub.WriteString(temp)
@@ -32,26 +40,35 @@ func decodeString(s string) string {
 	}
 
 	define_number = func(index int) (int, int) {
+
 		var number strings.Builder
+
 		for s[index] >= 48 && s[index] <= 57 {
 			number.WriteByte(s[index])
 			index++
 		}
+
 		num, _ := strconv.Atoi(number.String())
 		index++
+
 		return num, index
 	}
 
 	for i := 0; i < len(s); i++ {
+
 		if s[i] >= 49 && s[i] <= 57 {
+
 			number, next_index := define_number(i)
+
 			var sub string
 			sub, next_index = def(number, next_index)
+
 			result.WriteString(sub)
-			fmt.Println("result:", result.String())
 			i = next_index
+
 			continue
 		}
+        
 		if s[i] != '[' && s[i] != ']' {
 			result.WriteByte(s[i])
 		}
