ea9c311638e5e7d117748812ddeae0ffc043ad5b
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2026-03-26 16:47:52 +0300

    Time: 9 ms (26.99%), Space: 4.7 MB (75.69%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -0,0 +1,37 @@
+func lengthOfLongestSubstring(s string) int {
+	letterMap := make(map[byte]int)
+	lenght := 0
+	maxLen := 0
+	str := ""
+
+	l, r := 0, 0
+	for r < len(s) {
+		if _, exist := letterMap[s[r]]; exist {
+			temp := l
+			l = letterMap[s[r]] + 1
+			for i := temp; i < letterMap[s[r]]; i++ {
+				delete(letterMap, s[i])
+			}
+			l = letterMap[s[r]] + 1
+			lenght = r - l + 1
+			letterMap[s[r]] = r
+		} else {
+			letterMap[s[r]] = r
+			lenght = r - l + 1
+		}
+		if lenght > maxLen {
+			if r >= len(s) {
+				str = s[l:r]
+			} else {
+				str = s[l : r+1]
+			}
+
+			maxLen = lenght
+		}
+		r++
+	}
+
+	fmt.Println(str)
+
+	return maxLen
+}
