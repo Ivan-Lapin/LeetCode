a91afed6f26c9831ccf4d67a267cc699734c97be
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2026-04-06 15:55:06 +0300

    Time: 12 ms (75.34%), Space: 7 MB (6.7%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.go
@@ -0,0 +1,71 @@
+func minWindow(s string, t string) string {
+
+	if len(t) > len(s) {
+		return ""
+	} else if t == s {
+		return s
+	}
+
+	m := make(map[rune]int)
+
+	for i := range len(t) {
+		el := rune(t[i])
+		m[el]++
+	}
+
+	accordance := 0
+	var result string
+	miss := make([]int, 0, len(s))
+	minLen := len(s)
+
+	start := 0
+	for start < len(s) {
+		if _, exist := m[rune(s[start])]; exist {
+			break
+		} else {
+			start++
+		}
+	}
+
+	l := start
+	r := l
+
+	for r < len(s) {
+
+		el := rune(s[r])
+		if _, exist := m[el]; exist {
+			m[el]--
+			if m[el] >= 0 {
+				accordance++
+			}
+			if r != l {
+				miss = append(miss, r)
+			}
+		}
+
+		for accordance == len(t) {
+
+            if len(t) == 1 {
+				return t
+			}
+
+			if len(s[l:r]) < minLen {
+				minLen = len(s[l : r+1])
+				result = s[l : r+1]
+			}
+
+			if len(miss) != 0 {
+				m[rune(s[l])]++
+				if m[rune(s[l])] > 0 {
+					accordance--
+				}
+				l = miss[0]
+				miss = miss[1:]
+			}
+		}
+
+		r++
+	}
+
+	return result
+}
