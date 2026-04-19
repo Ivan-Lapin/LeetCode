111940d39519802376ea541000480e434ef07953
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2026-04-19 15:05:38 +0300

    Time: 4 ms (7.14%), Space: 5.8 MB (32.86%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.go
+++ 0260-single-number-iii/0260-single-number-iii.go
@@ -2,18 +2,16 @@ func singleNumber(nums []int) []int {
     m := make(map[int]int)
     for _, num := range nums {
         m[num]++
+        if m[num] == 2 {
+            delete(m, num)
+        }
     }
 
     result := make([]int, 2)
     index := 0
-    for key, val := range m {
-        if val == 1 {
-            result[index] = key
-            index++
-        }
-        if index == 2 {
-            break
-        }
+    for key, _ := range m {
+        result[index] = key
+        index++
     }
 
     return result
