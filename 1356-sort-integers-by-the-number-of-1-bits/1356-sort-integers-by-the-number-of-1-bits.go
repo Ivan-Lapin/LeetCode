899d59ab9ca71f664048c6dd9acbec13106f09db
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2026-04-19 14:53:14 +0300

    Time: 3 ms (36.61%), Space: 5.2 MB (99.67%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.go
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.go
@@ -1,37 +1,13 @@
 func sortByBits(arr []int) []int {
 
-    type Number struct {
-        num int
-        countBits int
-    }
-
-    counts := make([]Number, len(arr))
-    var x int
-    var count int
-
-    for i, num := range arr {
-        x = num
-        for x > 0 {
-            if x & 1 == 1{
-                count++
-            }
-            x>>=1
+    sort.Slice(arr, func(i, j int) bool {
+        c1 := bits.OnesCount(uint(arr[i]))
+        c2 := bits.OnesCount(uint(arr[j]))
+        if c1 == c2 {
+            return arr[i] < arr[j]
         }
-        counts[i] = Number{num: num, countBits: count}
-        count = 0
-    }
+        return c1 < c2
+    })
 
-    sort.Slice(counts, func(i, j int) bool {
-        if counts[i].countBits == counts[j].countBits {
-            return counts[i].num < counts[j].num
-        }
-		return counts[i].countBits < counts[j].countBits
-	})
-
-    result := make([]int, len(counts))
-    for i, number := range counts {
-        result[i] = number.num
-    }
-
-    return result
+    return arr
 }
