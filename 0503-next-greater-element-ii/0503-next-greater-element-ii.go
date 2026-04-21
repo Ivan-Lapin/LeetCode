235f61099223edd8a79f13ac8fce6045bc26264b
p1n41k <104844376+Ivan-Lapin@users.noreply.github.com>
2026-04-21 17:15:06 +0300

    Time: 2 ms (44.95%), Space: 8.9 MB (47.98%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.go
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.go
@@ -1,37 +1,20 @@
 func nextGreaterElements(nums []int) []int {
 
-	m := make(map[int][]int)
-	set := make(map[int]bool)
-
-	for i := range nums {
-		set[nums[i]] = true
-	}
-
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; ; j++ {
-			if j == len(nums) {
-				j = 0
-			}
-
-			if j == i {
-				break
-			}
-			if nums[i] < nums[j] {
-				m[nums[i]] = append(m[nums[i]], nums[j])
-				break
-			}
-		}
-	}
-
 	ans := make([]int, len(nums))
-	for i := range len(nums) {
-		if _, exist := m[nums[i]]; !exist || len(m[nums[i]]) == 0 {
-			ans[i] = -1
-		} else {
-			ans[i] = m[nums[i]][0]
-			m[nums[i]] = m[nums[i]][1:]
-		}
-	}
+    for i := range nums {
+        ans[i] = -1
+    }
+    stack := []int{}
+
+    for i := 0; i < len(nums)*2; i++{
+        idx := i%len(nums)
+        num := nums[idx]
+        for len(stack) > 0 && num > nums[stack[len(stack)-1]] {
+            ans[stack[len(stack)-1]] = num
+            stack = stack[:len(stack)-1]
+        }
+        stack = append(stack, idx)
+    }
 
 	return ans
 }
